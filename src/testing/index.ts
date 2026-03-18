4ae831aab5f7513aab110d1fe0ba483f16feb916
Arash Molavi <ironyh@gmail.com>
2026-03-18 02:18:06 +0100

    Export e2eHarness utilities from testing module
    - Added exports for isE2EMode, getE2EEmit, getEventBridge, setListenersReady
    - Added E2EEmitFn type export
    - Enables E2E test harness utilities for external consumers
    
    Refs: VueSIP-5iis

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -5,3 +5,10 @@
  */
 
 export { waitForCondition, waitForResult, waitForDefined, type WaitForOptions } from './waitFor'
+export {
+  isE2EMode,
+  getE2EEmit,
+  getEventBridge,
+  setListenersReady,
+  type E2EEmitFn,
+} from './e2eHarness'
